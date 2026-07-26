83f2803f7199a79e548ffece5b03ae5bc40d3faf
Miguel Flores Ccama <mif_112@hotmail.com>
2026-07-26 10:50:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-import pandas as pd
+import streamlit as st
+
+import numpy as np
+
+import libreria_funciones as lf
+
+st.session_state
+
+st.title("Proyecto módulo 1 Fundamentals")
+
+st.sidebar.title("Contenedor")
+
+ 
+
+st.image("Python_logo.png")
+
+st.sidebar.image("DMC.png")
+
+ 
+
+modulo = st.sidebar.selectbox("Elija un módulo", ["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
