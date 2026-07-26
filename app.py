d10c3c277e1bbe8a9f3cc528b19e6fb8e5e67ea1
Miguel Flores Ccama <mif_112@hotmail.com>
2026-07-26 12:02:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ modulo = "Módulo 1 – Python Fundamentals"
 anio = 2026
 informacion = "Soy Ing. de Sistemas que busca profundizar sus conocimientos en Python y en la ciencia de datos."
 descripcion = "El proyecto 1 pythn fundamentals busca fortalecer los conocimientos basicos en Python."
-herramientas = ["Python", "Streamli", "Numpy"]
+herramientas = ["Python", "Streamlit", "Numpy"]
 url_imagen = "https://raw.githubusercontent.com/miguel0810/Trabajo_Practico_Modulo_1_Miguel_Flores/506e02d1926c6ed86f4438f4cf821b6531b89f51/DMC.png"
 
 
 st.session_state
-st.sidebar.title("Contenedor")
+
 st.title(f"Titulo: {titulo}")
 st.image(url_imagen)
 st.subheader(f"Alumno: {alumno}")
@@ -25,9 +25,6 @@ st.markdown(f"Descripción del proyecto: {descripcion}")
 st.markdown(f"Las herramientas utilizadas para este proyecto fueron: {herramientas}")
 
 
-
+st.sidebar.title("Contenedor")
 st.sidebar.image("DMC.png")
-
- 
-
 modulo = st.sidebar.selectbox("Elija un módulo", ["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
