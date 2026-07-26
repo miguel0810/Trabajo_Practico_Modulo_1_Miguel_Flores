ebcba6c5d3e9a2c31ebabc360e6b563db9ee06e0
Miguel Flores Ccama <mif_112@hotmail.com>
2026-07-26 11:38:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,20 @@ anio = 2026
 informacion = "Soy Ing. de Sistemas que busca profundizar sus conocimientos en Python y en la ciencia de datos."
 descripcion = "El proyecto 1 pythn fundamentals busca fortalecer los conocimientos basicos en Python."
 herramientas = ["Python", "Streamli", "Numpy"]
+url_imagen = "https://raw.githubusercontent.com/miguel0810/Trabajo_Practico_Modulo_1_Miguel_Flores/506e02d1926c6ed86f4438f4cf821b6531b89f51/DMC.png"
+
 
 st.session_state
 st.sidebar.title("Contenedor")
 st.title(f"Titulo: {titulo}")
-st.image("https://github.com/miguel0810/Trabajo_Practico_Modulo_1_Miguel_Flores/blob/506e02d1926c6ed86f4438f4cf821b6531b89f51/DMC.png")
+st.image(url_imagen)
 st.subheader(f"Alumno: {alumno}")
 st.subheader(f"Modulo: {modulo}")
 st.write(f"Información general del estudiante: {informacion}"),
 st.write(f"Año: {anio}")
 st.markdown(f"Año: {anio}"),
 st.markdown(f"descripción del proyecto: {descripcion}"),
+st.markdown(f"**Las herramientas utilizadas para este proyecto fueron:** {herramientas_str}")
 st.markdown(f"Las herramientas utilizadas para este proyecto fueron: : {herramientas}")
 
 
