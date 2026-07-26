b292d3b5743329c3b49bf439b501352428ebe5f8
Miguel Flores Ccama <mif_112@hotmail.com>
2026-07-26 12:27:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # Importación de las librerías del proyecto
 import libreria_funciones_proyecto1 as lf
-import librería_clases_proyecto1 as lc
+import libreria_clases_proyecto1 as lc
 
 # Configuración de la página
 st.set_page_config(
